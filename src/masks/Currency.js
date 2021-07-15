e61ee43056e1711c41acd3dca40b76bb3edcc013
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2021-07-14 23:10:48 -0300

    Adicionar zero a esquerda quando o valor real vier da api com um zero oculto

--- src/masks/Currency.js
+++ src/masks/Currency.js
@@ -4,10 +4,12 @@ export class Currency {
             return value;
         }
 
-        return value.toString()
-                    .replace(/\D/g, "")
-                    .replace(/(\d)(\d{2})$/, "$1,$2")
-                    .replace(/(?=(\d{3})+(\D))\B/g, ".");
+        value = value.toString();
+
+        return value.replace(/(\d)\.(\d{1})$/, "$1.$20")
+            .replace(/\D/g, "")
+            .replace(/(\d)(\d{2})$/, "$1,$2")
+            .replace(/(?=(\d{3})+(\D))\B/g, ".");
     }
 
     unmount(value) {
