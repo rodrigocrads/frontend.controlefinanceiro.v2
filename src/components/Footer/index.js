c5e924791aea7dc9cfb870e23bfff575a05b4f25
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2021-09-23 15:09:34 -0300

    feat: Adicionar link para a rede social linkedin

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,6 +2,7 @@ function Footer() {
     return (
         <div id="rodape">
             © 2015 Rodrigo Cunha Rodrigues, All Rigths Reserved.
+            <p><a target='_blank' href="https://linkedin.com/in/rodrigo-cr-ads">Linkedin</a></p>
         </div>
     );
 }
