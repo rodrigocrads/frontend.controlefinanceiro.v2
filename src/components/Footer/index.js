4f02212a327c51b53a104587c72881b25522a250
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2021-09-24 11:24:54 -0300

    fix(Footer): Ajustar texto relacionado aos direitos de propriedade

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <div id="rodape">
-            © 2015 Rodrigo Cunha Rodrigues, All Rigths Reserved.
+            © 2015 Rodrigo Cunha Rodrigues, All Rights Reserved.
             <p><a target='_blank' href="https://linkedin.com/in/rodrigo-cr-ads">Linkedin</a></p>
         </div>
     );
