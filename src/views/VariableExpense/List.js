f7b0e43c1bdc5a6ae39edecd1e5f47534e415450
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2021-09-25 19:13:24 -0300

    feat(VariableExpense): criar campos e lógica de filtro de despesas variáveis

--- src/views/VariableExpense/List.js
+++ src/views/VariableExpense/List.js
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom';
 import icoList from '../../img/ico-list.png';
 import icoEdit from '../../img/edit.png';
 import icoDelete from '../../img/delete.png';
-import { convertCurrencyToPtBr, convertIsoDateToBr, fetchWithAuth } from '../../helpers/utils';
+import { convertCurrencyToPtBr, convertIsoDateToBr, fetchWithAuth, getCategoriesSelectOptions } from '../../helpers/utils';
+import Select from '../../components/UI/Select';
+import Input from '../../components/UI/Input';
+import { Date as DateMask } from '../../masks/Date';
 
 export default class ViewVariableExpenseList extends Component {
     constructor(props) {
@@ -12,10 +15,18 @@ export default class ViewVariableExpenseList extends Component {
 
         this.state = {
             variableExpenses: [],
+            categories: [],
+            form: {
+                title: '',
+                category_id: '',
+                start_date: '',
+                end_date: '',
+            }
         };
     }
 
     componentDidMount() {
+        this.fetchVariableExpenses();
         this.fetchCategories();
     }
 
@@ -24,11 +35,50 @@ export default class ViewVariableExpenseList extends Component {
         this.fetchCategories();
     }
 
-    fetchCategories() {
-        fetchWithAuth(`${process.env.REACT_APP_API_BASE_URL}variableExpense`)
+    getQueryParams() {
+        const queryParams = [];
+
+        if (this.state.form.title)
+            queryParams.push(`params[title]=${this.state.form.title}`);
+
+        if (this.state.form.category_id)
+            queryParams.push(`params[category_id]=${this.state.form.category_id}`);
+
+        if (this.state.form.start_date)
+            queryParams.push(`params[start_date]=${this.state.form.start_date}`);
+
+        if (this.state.form.end_date)
+            queryParams.push(`params[end_date]=${this.state.form.end_date}`);
+
+        return queryParams;
+    }
+
+    buildUrlWithQueryParams(url) {
+        const queryParams = this.getQueryParams();
+
+        for(var i = 0; i < queryParams.length; i++) {
+            if (i===0) {
+                url += queryParams[i];
+                continue;
+            }
+
+            url += `&${queryParams[i]}`;
+        }
+
+        return url;
+    }
+
+    fetchVariableExpenses() {
+        fetchWithAuth(this.buildUrlWithQueryParams(`${process.env.REACT_APP_API_BASE_URL}variableExpense?`))
             .then(response => response.json())
             .then(variableExpenses => this.setState({ ...this.state, variableExpenses }))
             .catch(e => { console.log(e) });
+    }
+
+    fetchCategories() {
+        fetchWithAuth(`${process.env.REACT_APP_API_BASE_URL}category?type=expense`)
+            .then(response => response.json())
+            .then(categories => this.setState({ ...this.state, categories }));
     }
 
     deleteCategoryHandler(id) {
@@ -46,6 +96,64 @@ export default class ViewVariableExpenseList extends Component {
                 this.resetCategoriesList();
             })
             .catch((error) => console.log(error));
+    }
+
+    onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        this.fetchVariableExpenses();
+    }
+
+    onChangeHandler = (event) => {
+        this.setState({ form: { ...this.state.form, [event.target.name]: event.target.value }});
+    }
+
+    renderFilterForm() {
+        return (
+            <form onSubmit={ this.onSubmitHandler }>
+                <div className="col_2 float_left">
+                    <Input
+                        label='TÍTULO:'
+                        name='title'
+                        value={ this.state.form.title }
+                        onChange={ this.onChangeHandler }
+                        maxLength='100'
+                    />
+
+                    <Select
+                        label="CATEGORIA:"
+                        name="category_id"
+                        value={ this.state.form.category_id }
+                        options={ getCategoriesSelectOptions(this.state.categories) }
+                        onChange={ this.onChangeHandler }
+                    />
+                </div>
+
+                <div className="col_2 float_left">
+                    <Input
+                        label='DATA INÍCIO:'
+                        name='start_date'
+                        value={ this.state.form.start_date }
+                        mask={new DateMask()}
+                        onChange={ this.onChangeHandler }
+                    />
+
+                    <Input
+                        label='DATA FIM:'
+                        name='end_date'
+                        value={ this.state.form.end_date }
+                        mask={new DateMask()}
+                        onChange={ this.onChangeHandler }
+                    />
+                </div>
+
+                <div className="form-actions">
+                    <div className="form-action">
+                        <input type="submit" className="btn" value="Buscar" />
+                    </div>
+                </div>
+            </form>
+        );
     }
 
     renderTable() {
@@ -89,13 +197,8 @@ export default class ViewVariableExpenseList extends Component {
         );
     }
 
-    renderNotFound() {
-        return (
-            <div>Nenhum registro encontrado!</div>
-        );
-    }
-
     render() {
+        const foundLength = this.state.variableExpenses.length;
         return (
             <div>
                 <div className="header_walk_links">
@@ -108,12 +211,17 @@ export default class ViewVariableExpenseList extends Component {
                     </div>
 
                     <div className="widget_content">
+                        <div className="filter_area">
+                            <h3>Filtros de busca:</h3>
+                            <br />
+                            { this.renderFilterForm() }
+                        </div>
+
                         <div className="table_area">
-                            {
-                                this.state.variableExpenses.length > 0
-                                ? this.renderTable()
-                                : this.renderNotFound()
-                            }
+                            <p>Encontrados: { foundLength === 1 ? `${foundLength} registro` : `${foundLength} registros` }, com o valor total de: { convertCurrencyToPtBr(this.state.variableExpenses.reduce((total, item) => item.value + total, 0)) } </p>
+                            <br />
+
+                            { this.state.variableExpenses.length > 0 ? this.renderTable() : '' }
                         </div>
                     </div>
                 </div>
