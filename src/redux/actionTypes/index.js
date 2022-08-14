c70fdf1dac3c10327fce66943e38a8d6327f63cf
Rodrigo Cunha <rodrigo.cr.ads@gmail.com>
2022-08-14 13:31:22 -0300

    fix: removendo import de arquivo inexistente

--- src/redux/actionTypes/index.js
+++ src/redux/actionTypes/index.js
@@ -2,14 +2,12 @@ import * as userTypes from './userTypes';
 import * as categoryTypes from './categoryTypes';
 import * as financialTransactionTypes from './financialTransactionTypes';
 import * as sidebarTypes from './sidebarTypes';
-import * as globalTypes from './globalTypes';
 
 const actionTypes = {
     ...categoryTypes,
     ...financialTransactionTypes,
     ...sidebarTypes,
     ...userTypes,
-    ...globalTypes,
 };
 
 export default actionTypes;
